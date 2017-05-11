a43d3490f7a9c4eaf1246b5c4fc6896a0f4cfc1f
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-11 11:25:55 -0700

    Add class model validations

--- benchbnb/spec/models/user_spec.rb
+++ benchbnb/spec/models/user_spec.rb
@@ -11,14 +11,31 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
     it do
-      should validate_length_of(:password).
-      is_at_least(6).
-      on(:create)
+      should validate_length_of(:password).is_at_least(6).on(:create)
     end
   end
 
   describe 'associations' do
     it { should have_many(:favorites) }
     it { should have_many(:favorite_benches) }
+  end
+
+  describe 'model_methods' do
+    describe '.find_by_credentials' do
+      let!(:user) { User.create(username: 'test2', password: 'password') }
+      context 'when given correct credentials' do
+        it 'should find the right user' do
+          correct_user = User.find_by_credentials('test2', 'password')
+          expect(correct_user).to eq(user)
+        end
+      end
+
+      context 'when given incorrect credentials' do
+        it 'should return nil' do
+          incorrect_user = User.find_by_credentials('test3', 'password')
+          expect(incorrect_user).to eq(nil)
+        end
+      end
+    end
   end
 end
